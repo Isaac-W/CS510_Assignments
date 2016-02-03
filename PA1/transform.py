4a234abf02dcee97a64d1acc9b89b2419ec83276
Isaac Wang <wangi@spu.edu>
2016-02-03 11:55:15 -0700

    Fix file not found check

--- PA1/transform.py
+++ PA1/transform.py
@@ -18,22 +18,22 @@ def main():
     output_path = args[2]
 
     # Read points file
-    with open(points_path) as points:
-        # Get src -> dst point mappings
-        src = []
-        dst = []
+    try:
+        with open(points_path) as points:
+            # Get src -> dst point mappings
+            src = []
+            dst = []
 
-        for line in points:
-            values = line.split()
-            if len(values) < 4:
-                # print 'Error: Less than 4 numerical values found in line:', line
-                continue
+            for line in points:
+                values = line.split()
+                if len(values) < 4:
+                    # print 'Error: Less than 4 numerical values found in line:', line
+                    continue
 
-            # TODO Catch unexpected non-integer values
-            src.append([int(values[1]), int(values[0])])    # Read values as  (x, y)
-            dst.append([int(values[3]), int(values[2])])    # Store values as (y, x) -- for OpenCV
-
-    if src is None:
+                # TODO Catch unexpected non-integer values
+                src.append([int(values[1]), int(values[0])])    # Read values as  (x, y)
+                dst.append([int(values[3]), int(values[2])])    # Store values as (y, x) -- for OpenCV
+    except:
         print 'Error--File not found:', points_path
         return
 
