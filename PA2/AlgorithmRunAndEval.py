82d25984f7270d380271e1da6029a3e313411da6
Josh Gillham <usajoshgillham@gmail.com>
2016-03-01 00:40:13 -0700

    Main program now can skip over x frames.

--- PA2/AlgorithmRunAndEval.py
+++ PA2/AlgorithmRunAndEval.py
@@ -51,8 +51,8 @@ def processFrame( model, writer, frame, height, width ):
     
     return combined, combined
 
-def processAndAnalyzeVideo( truth_cap, input_cap, csv_writer, no_out, no_csv,
-        doEval, model, writer, height, width ):
+def processAndAnalyzeVideo( truth_cap, input_cap, csv_writer, params,
+        model, writer, height, width ):
 
     # Calculate and display megapixels.
     megapixels = height * width / 1000000.0
@@ -65,6 +65,15 @@ def processAndAnalyzeVideo( truth_cap, input_cap, csv_writer, no_out, no_csv,
     # Init running totals
     frame_number = 0
     videoStats = e.TruthComparisonStats()
+    
+    # Fast forward past <params.startFrame> frames.
+    print "Skipping %d frames." % params.startFrame
+    for i in range( 0, params.startFrame ):
+        truth_ret = truth_cap.grab()
+        input_ret = input_cap.grab()
+
+        if not truth_ret or not input_ret:
+            raise Exception( "Start time past the end of videos." )
 
     try:
         # Keep track of the last input key, the frames, and time.
@@ -89,18 +98,18 @@ def processAndAnalyzeVideo( truth_cap, input_cap, csv_writer, no_out, no_csv,
             toShow, toFile = processFrame( 
                 model, writer, input_frame, height, width )
 
-            if doEval:
+            if params.doEval:
                 truthComparisonStats = \
                     e.CalculateFrameStats( height, width, truth_frame, toFile )
                 metaStats = e.TruthMetaComparisonStats( truthComparisonStats )
 
                 # Output running stats
-                if not no_out:
+                if not params.no_out:
                     truthComparisonStats.printOut( "Frame", frame_number)
                     metaStats.printOut( "     ", frame_number)
 
                 # Write running stats
-                if not no_csv:
+                if not params.no_csv:
                     frameStats = e.FrameStats(truthComparisonStats, metaStats)
                     csvArray = frameStats.GetCSVArray( "Frame", frame_number )
                     csv_writer.writerow( csvArray )
@@ -127,48 +136,61 @@ def processAndAnalyzeVideo( truth_cap, input_cap, csv_writer, no_out, no_csv,
         pass
     return frame_number, videoStats
 
+
+class Params:
+    def __init__(self, truth_path, input_path):
+        self.no_out = False
+        self.no_csv = False
+        self.doEval = False
+        self.startFrame = 0
+        self.truth_path = truth_path
+        self.input_path = input_path
+
+def readArgs( params ):
+    global no_out, no_csv, doEval, startFrame
+    i = MIN_ARGS + 1
+    while i < len(sys.argv):
+        currentArg = sys.argv[i]
+        if currentArg == '-e':
+            params.doEval = True
+        elif currentArg == '-s':
+            params.no_out = True
+        elif currentArg == '-n':
+            params.no_csv = True
+        elif currentArg == '-t':
+            i = i + 1
+            params.startFrame = int(sys.argv[i])
+        i = i + 1
+
 def main():
     if len(sys.argv) < MIN_ARGS + 1:
-        print 'usage: %s <ground truth> <input video> [-e] [-s|-n]' % sys.argv[0]
+        print ( 'usage: %s <ground truth> <input video> [-e] [-s|-n] ' + \
+            '[-t <startFrame> ]') % sys.argv[0]
         print '-----------------------------------------------'
         print 'flags:'
         print '       -e -- evaluate the results.'
         print '       -n -- no csv output; console stats only.'
         print '       -s -- silent; run without console output.'
+        print '       -t <startFrame> -- start running after <startFrame> frames.'
         return
 
-    truth_path = sys.argv[1]
-    input_path = sys.argv[2]
+    programParams = Params(truth_path = sys.argv[1], input_path = sys.argv[2] )
 
     # Split the extension of the source video.
-    sourceName, file_extension = os.path.splitext(input_path)
-
-    no_out = False
-    no_csv = False
-    doEval = False
-
-    if len(sys.argv) > MIN_ARGS + 1:
-        flag = sys.argv[3].lower()
-        if flag == '-e':
-            doEval = True
-
-    if len(sys.argv) > MIN_ARGS + 2:
-        flag = sys.argv[4].lower()
-        if flag == '-s':
-            no_out = True
-        elif flag == '-n':
-            no_csv = True
+    sourceName, file_extension = os.path.splitext(programParams.input_path)
+    
+    readArgs( programParams )
 
     # Open the videos
-    truth_cap = cv2.VideoCapture(truth_path)
-    input_cap = cv2.VideoCapture(input_path)
+    truth_cap = cv2.VideoCapture(programParams.truth_path)
+    input_cap = cv2.VideoCapture(programParams.input_path)
 
     csv_file = None
     csv_writer = None
 
     # Create CSV writer
-    if not no_csv:
-        csv_path, extension = os.path.splitext(input_path)
+    if not programParams.no_csv:
+        csv_path, extension = os.path.splitext(programParams.input_path)
         csv_writer = e.createCSVWriter( csv_path )
 
         # Write headers
@@ -197,19 +219,19 @@ def main():
     print "init time: %g" % totalTime
 
     frame_number, videoStats = \
-        processAndAnalyzeVideo( truth_cap, input_cap, csv_writer, no_out,no_csv,
-            doEval, model, writer, height, width )
+        processAndAnalyzeVideo( truth_cap, input_cap, csv_writer, programParams, 
+            model, writer, height, width )
 
     # Calculate frame averages
-    if doEval:
+    if programParams.doEval:
         results = e.calculateResults( frame_number, videoStats )
 
     # Output totals and averages
-    if not no_out and doEval:
+    if not programParams.no_out and programParams.doEval:
         e.printSummary( frame_number, results )
 
     # Write video stats
-    if not no_csv and doEval:
+    if not programParams.no_csv and programParams.doEval:
         e.writeSummaryToCSV( csv_writer, frame_number, results )
 
     # Cleanup
